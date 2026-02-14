41ef1e31023e79f3eea63353c474fe1b77f63724
Shadow Monarch <shadow-monarch@shadow-monarch.Shadow>
2026-02-14 22:31:20 +0600

    layout readded- Done

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,6 +1,6 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import React, { ReactNode } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 interface LayoutProps {
   children: ReactNode;
