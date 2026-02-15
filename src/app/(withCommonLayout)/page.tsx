945563bccf1b7ce648136c5e9e461f8e4ecc1f4d
Abu Rahian <aburaihan363@gmail.com>
2026-02-15 17:17:43 +0600

    [UPDATED]-pricipal-message

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,12 +1,15 @@
-import NoticeBoard from '@/components/home/NoticeBoard/NoticeBoard';
-import React from 'react';
+import NoticeBoard from "@/components/home/NoticeBoard/NoticeBoard";
+import PrincipalMessage from "@/components/home/PricipalMessage/PricipalMessage";
+
+import React from "react";
 
 const HomePage = () => {
-    return (
-       <div className="space-y-10">
-            <NoticeBoard />
-        </div>
-    );
+  return (
+    <div className="space-y-10">
+      <PrincipalMessage />
+      <NoticeBoard />
+    </div>
+  );
 };
 
 export default HomePage;
