74a284d26a4cfeaa03455ff135f670faf5d23c57
mdmahfuzahmedony <Mdmahfuzahmedony@gmail.com>
2026-02-16 20:34:46 +0600

    [ADDED]: TEAM-MEMBER SECTION

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,11 +2,13 @@ import NoticeBoard from "@/components/home/NoticeBoard/NoticeBoard";
 import PrincipalMessage from "@/components/home/PricipalMessage/PricipalMessage";
 
 import React from "react";
+import Teampage from "../allpages/team/page";
 
 const HomePage = () => {
   return (
     <div className="space-y-10">
-      <PrincipalMessage />
+      <Teampage></Teampage>
+      {/* <PrincipalMessage /> */}
       <NoticeBoard />
     </div>
   );
