c2a54a226ac4454664c061d4a149f113edd89dde
Shahriar Refat <shahariarrefat@gmail.com>
2026-03-01 01:50:02 +0600

    fix error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -7,7 +7,22 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("DIRECT_URL")
+    // url: env("DATABASE_URL"),
+    // shadowDatabaseUrl: env("DIRECT_URL")
+       url: process.env.DATABASE_URL!,
+    directUrl: process.env.DIRECT_URL!,
   },
 });
+
+
+// Please do:
+//before start your code.
+// 1️⃣ git pull origin development
+// 2️⃣ npx prisma generate
+// 3️⃣ npx prisma migrate dev
+
+
+
+// If you get drift/conflict error, run:
+
+// npx prisma migrate reset
