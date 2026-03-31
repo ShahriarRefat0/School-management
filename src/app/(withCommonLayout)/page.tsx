7e587650e4d4448064796aab52675d333b63cd0d
mdmahfuzahmedony <Mdmahfuzahmedony@gmail.com>
2026-03-31 16:05:48 +0600

    UPDATE HOME PAGE

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,15 +1,17 @@
 import HeroSection from "@/components/home/heroSection/HeroSection";
 import TestimonialsSection from "@/components/home/TestimonialsSection/TestimonialsSection";
 import Features from "@/components/home/Features/Features";
+
 import HowItWorks from "@/components/home/howitWorks/howitWorks";
 import FAQ from "@/components/home/FAQ/FAQ";
 import BlogSection from "@/components/home/blogSection/blog";
 import WhyChooseUs from "@/components/home/WhyChooseUs/WhyChooseUs";
 import ExploreOurDashboardFeatures from "@/components/home/ExploreOurDashboardFeatures/ExploreOurDashboardFeatures";
-<<<<<<< HEAD
+
 import { FloatingClock } from "@/components/shared/FloatingClock";
-=======
->>>>>>> 73e65a5 (chenge some layout)
+
+
+
 
 const HomePage = () => {
   return (
@@ -17,17 +19,23 @@ const HomePage = () => {
       <HeroSection></HeroSection>
       <WhyChooseUs />
       <ExploreOurDashboardFeatures />
-<<<<<<< HEAD
-      {/* <PrincipalMessage /> */}
-=======
-      <PrincipalMessage />
->>>>>>> 73e65a5 (chenge some layout)
+
+
+   
       <Features />
       <HowItWorks></HowItWorks>
       <TestimonialsSection></TestimonialsSection>
       <FAQ></FAQ>
       <BlogSection></BlogSection>
       <FloatingClock />
+
+      <Features />
+      <HowItWorks></HowItWorks>
+
+      <TestimonialsSection></TestimonialsSection>
+      <FAQ></FAQ>
+      <BlogSection></BlogSection>
+
     </div>
   );
 };
