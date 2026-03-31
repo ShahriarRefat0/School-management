267f1014ea71406c2b2e799828f4061b289335ae
Shahriar Refat <shahariarrefat@gmail.com>
2026-04-01 00:00:12 +0600

    fix error

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -6,7 +6,10 @@ import FAQ from "@/components/home/FAQ/FAQ";
 import BlogSection from "@/components/home/blogSection/blog";
 import WhyChooseUs from "@/components/home/WhyChooseUs/WhyChooseUs";
 import ExploreOurDashboardFeatures from "@/components/home/ExploreOurDashboardFeatures/ExploreOurDashboardFeatures";
+<<<<<<< HEAD
 import { FloatingClock } from "@/components/shared/FloatingClock";
+=======
+>>>>>>> 73e65a5 (chenge some layout)
 
 const HomePage = () => {
   return (
@@ -14,7 +17,11 @@ const HomePage = () => {
       <HeroSection></HeroSection>
       <WhyChooseUs />
       <ExploreOurDashboardFeatures />
+<<<<<<< HEAD
       {/* <PrincipalMessage /> */}
+=======
+      <PrincipalMessage />
+>>>>>>> 73e65a5 (chenge some layout)
       <Features />
       <HowItWorks></HowItWorks>
       <TestimonialsSection></TestimonialsSection>
